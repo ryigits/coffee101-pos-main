275b6c8bcd2632db132be8d22314e229cba3a5a2
Yigit <ryigit@gmail.com>
2022-08-22 22:51:31 +0200

    first commit

--- client/src/start.js
+++ client/src/start.js
@@ -1,36 +1,10 @@
 import { createRoot } from "react-dom/client";
-import Welcome from "./Welcome";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
-import * as immutableState from "redux-immutable-state-invariant";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./redux/reducer.js";
-import { Provider } from "react-redux";
-import { init } from "./socket.js";
-
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(immutableState.default()))
-);
 
 const main = document.querySelector("main");
 const root = createRoot(main);
 
+root.render(
+        <App />
 
-
-fetch("/user/id.json")
-    .then((response) => response.json())
-    .then((data) => {
-        if (!data.userId) {
-            // this means that the user doens't have a userId and should see Welcome/Registration for now
-            root.render(<Welcome />);
-        } else {
-            // this means the user is registered cause their browser DID have the right cookie and they should be seeing a logo
-            init(store);
-            root.render(
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            );
-        }
-    });
+);
--- server/server.js
+++ server/server.js
@@ -2,15 +2,8 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
-const db = require("./db");
-const bcrypt = require("./bcrypt");
-const cryptoRandomString = require("crypto-random-string");
-const ses = require("./ses");
-const { uploader } = require("./multer");
-const s3 = require("./s3");
-// const { userLogIn, userLogOut } = require("./middleware");
-app.use(express.json());
 
+app.use(express.json());
 ////// this is our socket.io boilerplate  //////
 const server = require("http").Server(app);
 let localOrHeroku;
@@ -53,222 +46,17 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
-app.get("/user/id.json", function (req, res) {
-    res.json({
-        userId: req.session.id,
-    });
-});
-
-app.get("/profile", function (req, res) {
-    db.getUserById(req.session.id).then((userData) => {
-        const { first_name, last_name, profilepic, bio, email } =
-            userData.rows[0];
-        res.json({
-            id: req.session.id,
-            first: first_name,
-            last: last_name,
-            bio: bio,
-            url: profilepic,
-            email: email,
-        });
-    });
-});
-
-app.get("/closefriends/getall", async (req, res) => {
-    const allFriend = await db.getAllCloseFriendsAndWannabes(req.session.id);
-    res.json(allFriend.rows);
-});
-
-app.get("/getalldirectmessages/:userId", async (req, res) => {
-    const { userId } = req.params;
-    const messages = await db
-        .findDirectMessages(userId, req.session.id)
-        .then((result) => result.rows);
-    res.json(messages);
-});
-
-app.get("/recentusers", (req, res) => {
-    db.getRecentUsers().then((users) => {
-        const filteredUser = users.rows.filter(
-            (user) => user.id != req.session.id
-        );
-        res.json(filteredUser);
-    });
-});
-
-app.get("/user/:firstName.json", (req, res) => {
-    const { firstName } = req.params;
-    db.getSearchUsers(firstName).then((userData) => {
-        const filteredUser = userData.rows.filter(
-            (user) => user.id != req.session.id
-        );
-        res.json(filteredUser);
-    });
-});
-
-app.get("/users/:id.json", (req, res) => {
-    const { id } = req.params;
-    if (Number(req.params.id) === req.session.id) {
-        return res.json({ forbidden: true });
-    } else {
-        return db.getUserById(id).then((data) => {
-            const userData = data.rows[0];
-            res.json(userData);
-        });
-    }
-});
-app.get("/friends/:id.json", async (req, res) => {
-    const { id } = req.params;
-    const _friends = await db
-        .findCloseFriendsById(id)
-        .then((result) => result.rows);
-    const friends = _friends.filter((e) => e.arefriend);
-    const friendsId = friends
-        .filter((e) => e.sender_id != +id)
-        .map((e) => e.sender_id);
-    const friendsId2 = friends
-        .filter((e) => e.receiver_id != +id)
-        .map((e) => e.receiver_id);
-    const finalIds = [...friendsId, ...friendsId2];
-    const result = await db.getUsersByIds(finalIds).then((data) => data.rows);
-    res.json(result);
-});
-
-app.get("/closefriend/:id.json", (req, res) => {
-    const user1 = req.session.id;
-    const user2 = req.params.id;
-    db.findCloseFriends(user1, user2).then((data) => {
-        if (data.rowCount === 0) {
-            return res.json({ result: "not found" });
-        } else {
-            res.json(data.rows[0]);
-        }
-    });
-});
-
-app.post("/addCloseFriend", async (req, res) => {
-    const result = await db.addCloseFriend(req.session.id, req.body.id);
-    res.json(result.rows[0]);
-});
-app.post("/removeCloseFriend", async (req, res) => {
-    await db.removeCloseFriend(req.body.id, req.session.id);
-    res.json({ result: "not found" });
-});
-app.post("/acceptCloseFriend", async (req, res) => {
-    const result = await db.acceptCloseFriend(req.body.id, req.session.id);
-    res.json(result.rows[0]);
-});
-
-app.post("/bio", (req, res) => {
-    db.updateBio(req.session.id, req.body.bioData).then(() => {
-        res.json({ success: true });
-    });
-});
-
-app.post("/register.json", (req, res) => {
-    const { first, last, email, password } = req.body;
-
-    bcrypt.hash(password).then((password_hash) => {
-        db.addUser(first, last, email, password_hash)
-            .then((user_id) => {
-                req.session.id = user_id.rows[0].id;
-                res.json({
-                    success: true,
-                    id: user_id.rows[0].id,
-                });
-            })
-            .catch(() => {
-                res.json({
-                    success: false,
-                });
-            });
-    });
-});
-
-app.post("/sendCodeBeforeResetPassword", (req, res) => {
-    const { email } = req.body;
-    const code = cryptoRandomString({ length: 4 });
-
-    db.addCodeIntoDb(email, code).then(() => {
-        ses.sendCodeToEmail(code, email).then(() => {
-            res.json({
-                email: email,
-                code: code,
-            });
-        });
-    });
-});
-
-app.post("/resetPasswordWithCode", (req, res) => {
-    const { code, newPassword, email } = req.body;
-    db.getCodesFromDb().then((codes) => {
-        if (codes.rows.map((item) => item.code).includes(code)) {
-            bcrypt.hash(newPassword).then((password_hash) => {
-                db.changePasswordByEmail(email, password_hash).then(() => {
-                    res.json({
-                        success: true,
-                    });
-                });
-            });
-        }
-    });
-});
-
-app.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    db.authUser(email, password)
-        .then((result) => {
-            if (result) {
-                db.getUserByEmail(email).then((user) => {
-                    req.session.id = user.id;
-                    res.json({
-                        success: true,
-                    });
-                });
-            } else {
-                res.json({
-                    message: "wrong email or password",
-                    success: false,
-                });
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.post("/image", uploader.single("photo"), s3.upload, (req, res) => {
-    const url = `https://frienznetwork.s3.amazonaws.com/${req.file.filename}`;
-    if (req.file) {
-        db.addProfilePic(req.session.id, url).then(() => {
-            res.json({
-                success: true,
-                url: url,
-            });
-        });
-    }
-});
-
-app.post("/deleteuser", async (req, res) => {
-    const { email } = req.body;
-    const match = await db.getUserByEmail(email);
-    if (match.id === req.session.id) {
-        await db.deleteUserByEmail(email).then((data) => data.rows);
-        req.session = null;
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
-});
-
-app.get("/logout", (req, res) => {
-    req.session = null;
-    res.json({ logout: true });
-});
-
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
+
+
+
+
+
+
+
+
 
 server.listen(process.env.PORT || 3001, function () {
     console.log("I'm listening.");

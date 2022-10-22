91d8134c5ae5279f0f92953a3412eedd5321d6b6
Yigit <ryigit@gmail.com>
2022-10-22 19:55:15 +0200

    again render.com

--- server/server.js
+++ server/server.js
@@ -60,5 +60,5 @@ app.get("*", function (req, res) {
 
 
 app.listen(process.env.PORT || 3001, function () {
-    console.log("I'm listening.");
+    console.log(`I'm listening on ${process.env.PORT}`);
 });
